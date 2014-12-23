c25786d044de3af0349501cdd68f0e182d45b8f7
Peksa <git@peksa.se>
2014-12-24 00:48:47 +0100

    Example that connects to IRC and joins a channel

--- src/lojjebot.go
+++ src/lojjebot.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	irc "github.com/fluffle/goirc/client"
+)
 
 func main() {
-	fmt.Printf("Hello world!")
+	fmt.Println("Booting up!")
+
+	// Creating a simple IRC client is simple.
+	c := irc.SimpleClient("lojjebot")
+
+	// Add handlers to do things here!
+	// e.g. join a channel on connect.
+	c.HandleFunc("connected",
+		func(conn *irc.Conn, line *irc.Line) {
+			fmt.Println("Joining #lojjebot")
+			conn.Join("#lojjebot")
+		})
+
+	// And a signal on disconnect
+	quit := make(chan bool)
+	c.HandleFunc("disconnected",
+		func(conn *irc.Conn, line *irc.Line) {
+			quit <- true
+		})
+
+	// Connect!
+	c.ConnectTo("irc.motherland.nu")
+
+	// Wait for disconnect
+	<-quit
 }
